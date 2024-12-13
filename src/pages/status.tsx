5125dcbc351da660d6b5ad92eef4c431c6834f6a
Gabriel Rodrigues <138010846+gabsrodrigues-dev@users.noreply.github.com>
2024-12-13 10:42:16 -0300

    up

---
+++ src/pages/status.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col w-full items-center justify-center gap-12">
+      <div className={`cursor-pointer text-[#FAFF00]`}>ok.</div>
+    </main>
+  );
+}
